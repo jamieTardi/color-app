a0f979e4dbbb56c65a8253ffc66e17d10de251ac
jamieTardi <jamie.tardi@gmail.com>
2020-08-30 15:20:51 +0100

    added brightness and sat

--- scripts/app.js
+++ scripts/app.js
@@ -20,12 +20,44 @@ randomColors = () => {
         //Add the color to the background
         div.style.background = randomColor;
         hextText.innerText = randomColor
-        
+        //contrast checking
+        checkTextContrast(randomColor, hextText)
+        //Intialize Color Sliders
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll('.sliders input');
+        console.log(sliders)
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2]
+
+        colorizeSliders (color, hue, brightness, saturation);
     })
 }
 
+//White text on a dark background and vice versa.
 checkTextContrast = (color, text) => {
     const luminance = chroma(color).luminance();
+    if (luminance > 0.5){
+        text.style.color = 'black'
+    }
+    else{
+        text.style.color = 'white'
+    }
+}
+
+colorizeSliders = (color, hue, brightness, saturation) => {
+    //Saturation Scale
+    const noSat = color.set('hsl.s', 0)
+    const fullSat = color.set('hsl.s', 1)
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+    //scale Brightness
+    const midBright = color.set('hsl.l', 0.5);
+    const scaleBright = chroma.scale (['black', midBright, 'white'])
+
+    //Update the input colors
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`
+    brightness.style.backgroundImage =  `linear-gradient(to right, ${scaleBright(1)}, ${scaleBright(0.5)} , ${scaleBright(0)})`
 }
 
 
